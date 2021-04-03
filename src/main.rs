f47484d9a77092417e63c1ccb86990ae8a62cd72
Makoto Hiramatsu <himkt@cookpad.com>
2021-04-03 22:41:54 +0900

    Impl

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,8 @@ struct Opt {
 }
 
 
-#[tokio::main]
-async fn main() -> octocrab::Result<()> {
+async fn create_pull_request_list() -> octocrab::Result<HashMap<String, Vec<String>>, octocrab::Error> {
     let opt: Opt = Opt::from_args();
-
     let octocrab = octocrab::instance();
     let page = octocrab
         .pulls("himkt", "konoha")
@@ -27,7 +25,7 @@ async fn main() -> octocrab::Result<()> {
         .direction(Direction::Descending)
         .sort(pulls::Sort::Created)
         .page(1 as u8)
-        .per_page(50 as u8)
+        .per_page(100 as u8)
         .send()
         .await?;
 
@@ -37,37 +35,62 @@ async fn main() -> octocrab::Result<()> {
     let mut page = Some(page);
     while let Some(current_page) = page {
         for pull in current_page.items {
-            println!("{} (#{})", pull.title, pull.number);
+            match pull.milestone {
+                Some(milestone) => {
+                    if milestone.title != opt.milestone {
+                        continue
+                    }
+                },
+                _ => {
+                    println!("!!no milestone found for #{}!!", pull.number)
+                },
+            }
 
+            println!("medium pr #{}, #{}", pull.number, opt.last_pull_request);
             match pull.labels {
                 Some(labels) => {
                     if labels.len() > 1 {
-                        println!("!!one more labels are assigned!!");
+                        println!("!!one more labels are assigned for #{}!!", pull.number);
                     }
 
                     for label in labels {
-                        println!("{}", label.name);
+                        if items_map.contains_key(&label.name) {
+                            items_map.get_mut(&label.name).unwrap().push(format!("{} (#{})", pull.title, pull.number));
+                        }
+                        else {
+                            items_map.insert(label.name, vec![format!("{} (#{})", pull.title, pull.number)]);
+                        }
                     }
                 },
-                _ => println!("!!no label found!!"),
-            }
-
-            match pull.milestone {
-                Some(milestone) => {
-                    println!("{}", milestone.title);
+                _ => {
+                    println!("!!no label found for #{}!!", pull.number)
                 },
-                _ => println!("!!no label found!!"),
             }
 
             // end of pull requests
             if pull.number == opt.last_pull_request {
-                return Ok(());
+                return Ok(items_map);
             }
         }
 
         // next cursor
         page = octocrab.get_page::<models::pulls::PullRequest>(&current_page.next).await?;
     }
+    Ok(items_map)
+}
 
+
+#[tokio::main]
+async fn main() -> octocrab::Result<()> {
+    let items_map = create_pull_request_list().await?;
+
+    println!("\n\n\n");
+    for (k, v) in items_map.iter() {
+        println!("#{}", k);
+        for pull in v.iter() {
+            println!("- {}", pull);
+        }
+        println!();
+    }
     Ok(())
 }
